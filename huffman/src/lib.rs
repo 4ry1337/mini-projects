f03fb1026cb8843a69b6fb9332c13c915b44e840
4ry1337 <yskak.rakhat@gmail.com>
2024-06-19 00:42:50 +0500

    huffman code and table

--- huffman/src/lib.rs
+++ huffman/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{BinaryHeap, HashMap};
 
 fn frequency(data: &str) -> HashMap<char, i32> {
     let mut weights = HashMap::new();
@@ -10,25 +10,56 @@ fn frequency(data: &str) -> HashMap<char, i32> {
 }
 
 #[derive(PartialEq, Eq, Debug)]
-pub struct Node {
-    frequency: i32,
-    value: Option<i32>,
-    left: Option<Box<Node>>,
-    right: Option<Box<Node>>,
+pub struct HuffmanNode {
+    frequency: usize,
+    value: Option<u8>,
+    left: Option<Box<HuffmanNode>>,
+    right: Option<Box<HuffmanNode>>,
 }
 
-impl Node {
-    pub fn new(frequency: i32, value: Option<i32>) -> Self {
+impl HuffmanNode {
+    // create leaf with value
+    pub fn leaf(value: u8, frequency: usize) -> Self {
         Self {
-            value,
+            value: Some(value),
             frequency,
             left: None,
             right: None,
         }
     }
+
+    //create node with childs
+    pub fn node(frequency: usize, right: HuffmanNode, left: HuffmanNode) -> Self {
+        Self {
+            frequency,
+            value: None,
+            left: Some(Box::new(left)),
+            right: Some(Box::new(right)),
+        }
+    }
+
+    pub fn from_frequencies(frequencies: [usize; 256]) -> Option<Box<Self>> {
+        let mut queue = BinaryHeap::new();
+        for i in 0..256 {
+            if frequencies[i] > 0 {
+                queue.push(Self::leaf(i as u8, frequencies[i]))
+            }
+        }
+
+        while queue.len() > 1 {
+            if let (Some(left), Some(right)) = (queue.pop(), queue.pop()) {
+                queue.push(Self::node(left.frequency + right.frequency, right, left))
+            }
+        }
+
+        match queue.pop() {
+            None => None,
+            Some(root) => Some(Box::new(root)),
+        }
+    }
 }
 
-impl Ord for Node {
+impl Ord for HuffmanNode {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         other
             .frequency
@@ -37,36 +68,200 @@ impl Ord for Node {
     }
 }
 
-impl PartialOrd for Node {
+impl PartialOrd for HuffmanNode {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(&other))
     }
 }
 
-pub fn compression(data: &str) -> String {
-    let weights = frequency(data);
-    println!("{:#?}", weights);
-    return String::new();
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct HuffmanCode {
+    value: u8,
+    frequency: usize,
+    length: usize,
+    bits: u128,
 }
 
-// pub fn decoding(data: &str) -> String {
-// }
-//
+impl Ord for HuffmanCode {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.length
+            .cmp(&other.length)
+            .then(self.value.cmp(&other.value))
+    }
+}
+
+impl PartialOrd for HuffmanCode {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(&other))
+    }
+}
+
+impl HuffmanCode {
+    pub fn from_tree(tree: &Option<Box<HuffmanNode>>) -> Vec<Self> {
+        fn collect(
+            output: &mut Vec<HuffmanCode>,
+            node: &Option<Box<HuffmanNode>>,
+            indent: usize,
+            bits: u128,
+        ) {
+            if let Some(node) = node {
+                if let Some(value) = node.value {
+                    output.push(HuffmanCode {
+                        value,
+                        frequency: node.frequency,
+                        length: indent,
+                        bits,
+                    });
+                }
+
+                // adds 0 to the end
+                collect(output, &node.left, indent + 1, bits << 1);
+
+                // adds 1 to the end
+                collect(output, &node.right, indent + 1, bits << 1 | 0x1);
+            }
+        }
+
+        let mut result = Vec::with_capacity(256);
+
+        collect(&mut result, &tree, 0, 0);
+
+        result
+    }
+
+    pub fn describe(codes: &Vec<Self>) {
+        for code in codes.iter() {
+            println!(
+                "{}, {:>3} {} {} {:0width$b}",
+                code.value as char,
+                code.value,
+                code.frequency,
+                code.length,
+                code.bits,
+                width = code.length
+            )
+        }
+    }
+
+    pub fn as_canonical(codes: &Vec<Self>) -> Vec<Self> {
+        let mut sorted: Vec<Self> = codes.iter().cloned().collect();
+
+        sorted.sort();
+
+        let mut bits = 0;
+        let mut length = 0;
+
+        for code in sorted.iter_mut() {
+            while length < code.length {
+                bits <<= 1;
+                length += 1;
+            }
+
+            code.bits = bits;
+            bits += 1;
+        }
+
+        sorted
+    }
+
+    pub fn encode(data: &str) -> String {
+        let result = String::new();
+
+        let mut frequencies = [0; 256];
+
+        for key in data.as_bytes().iter() {
+            frequencies[*key as usize] += 1;
+        }
+
+        let tree = HuffmanNode::from_frequencies(frequencies);
+
+        let codes = HuffmanCode::from_tree(&tree);
+
+        let canonical = HuffmanCode::as_canonical(&codes);
+
+        HuffmanCode::describe(&canonical);
+
+        for i in data.as_bytes() {
+            let x = canonical.iter().find(|&x| x.value == *i).unwrap().bits;
+            println!("{:b}", x);
+        }
+
+        let table = HuffmanTable::from_codes(&canonical);
+
+        result
+    }
+}
+
+#[derive(Debug)]
+pub struct HuffmanTable {
+    counts: [u8; 8],
+    symbols: [u8; 32],
+}
+
+impl HuffmanTable {
+    pub fn from_codes(codes: &Vec<HuffmanCode>) -> Self {
+        let mut counts = [0; 8];
+        let mut symbols = [0; 32];
+        let mut offset = 0;
+
+        for code in codes.iter() {
+            counts[code.length as usize] += 1;
+            symbols[offset] = code.value;
+            offset += 1;
+        }
+
+        Self { counts, symbols }
+    }
+
+    pub fn decode(&self, bits: u128) -> Option<u8> {
+        let mut length = 1;
+        let mut first: u128 = 0;
+        let mut bits = bits;
+
+        let mut code = 0;
+        let mut count: u128 = 0;
+        let mut offset: u128 = 0;
+
+        while length < 8 {
+            code |= bits & 0x1;
+            count = self.counts[length] as u128;
+
+            if code < first + count {
+                return Some(self.symbols[offset as usize + (code - first) as usize]);
+            }
+
+            offset += count;
+            first += count;
+            length += 1;
+
+            first <<= 1;
+            code <<= 1;
+            bits >>= 1;
+        }
+
+        None
+    }
+
+    pub fn describe(&self) {
+        println!("Counts:  {:?}", self.counts);
+        println!("Symbols: {:?}", self.symbols);
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::frequency;
+    use crate::HuffmanCode;
 
-    #[test]
-    fn frequency_test() {
-        let weights = frequency("HELLO WORLD");
-        assert_eq!(weights.get(&'O'), Some(&2));
-        assert_eq!(weights.get(&'L'), Some(&3));
-    }
     // #[test]
-    // fn encoding_test() {
-    //     let compressed = compression("A DEAD DAD CEDED A BAD BABE A BEADED ABACA BED");
-    //     assert_eq!(compressed, "00100010101110110101111101101100")
+    // fn frequency_test() {
+    //     let weights = frequency("HELLO WORLD");
+    //     assert_eq!(weights.get(&'O'), Some(&2));
+    //     assert_eq!(weights.get(&'L'), Some(&3));
     // }
+    #[test]
+    fn compression_test() {
+        let encoded = HuffmanCode::encode("the quick brown fox jumps over the lazy dog");
+    }
     // #[test]
     // fn decoding_test() {
     // }
