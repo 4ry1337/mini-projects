1906ed6505f5a8e28aae23e7628e2803f7c0dddc
Rakhat Yskak <yskak.rakhat@gmail.com>
2024-08-21 03:45:44 +0500

    [conway] finished working example

--- game-of-life/src/conway.rs
+++ game-of-life/src/conway.rs
@@ -1,38 +1,161 @@
-use crate::ascii_buffer::scene::Scene;
+use std::{thread::sleep, time::Duration};
+
+use rand::{rngs::StdRng, Rng, SeedableRng};
+
+pub const DEATH: &'static str = "\x1B[47m  \x1B[0m"; //WHITE
+pub const LIFE: &'static str = "\x1B[42m  \x1B[0m"; //GREEN
 
 pub struct Conway {
-    seed: usize,
-    life_count: usize,
+    seed: u64,
+    rng: StdRng,
+    population: usize,
+    height: usize,
+    width: usize,
     tick: usize,
-    scene: Scene,
+    grid: Vec<bool>,
+    next_grid: Vec<bool>,
 }
 
 impl Conway {
-    pub fn new(seed: usize, life_count: usize, height: usize, width: usize) -> Self {
+    pub fn new(seed: u64, population: usize, height: usize, width: usize) -> Self {
+        let rng = StdRng::seed_from_u64(seed);
+        let size = height * width;
         Self {
             seed,
-            life_count,
+            rng,
+            height,
+            width,
+            population,
             tick: 0,
-            scene: Scene::new(height, width),
+            grid: vec![false; size],
+            next_grid: vec![false; size],
         }
     }
 
-    pub fn seed(&mut self, seed: usize) {
-        self.seed = seed;
+    pub fn get(&self, row: usize, col: usize) -> Option<&bool> {
+        self.grid.get(row * self.width + col)
     }
+
+    pub fn set(&mut self, row: usize, col: usize, b: bool) {
+        self.grid[row * self.width + col] = b;
+    }
+
+    fn get_next(&self, row: usize, col: usize) -> Option<&bool> {
+        self.next_grid.get(row * self.width + col)
+    }
+
+    fn set_next(&mut self, row: usize, col: usize, b: bool) {
+        self.next_grid[row * self.width + col] = b;
+    }
+
+    pub fn seed(&mut self, seed: u64) {
+        self.seed = seed;
+        self.rng = StdRng::seed_from_u64(seed);
+    }
+
+    pub fn random_generation(&mut self) {
+        let mut population = self.population;
+        let mut row;
+        let mut col;
+        while population != 0 {
+            row = self.rng.gen_range(0..self.height);
+            col = self.rng.gen_range(0..self.width);
+            if !self.get(row, col).unwrap() {
+                self.set(row, col, true);
+                population -= 1;
+            }
+        }
+    }
+
+    // 0 1 2
+    // 3 4 5
+    // 6 7 8
+    //
+    // 00 01 02
+    // 10 11 12
+    // 20 21 22
 
     pub fn neighbour_count(&self, row: usize, col: usize) -> usize {
         let mut count: usize = 0;
-        for i in 0..=2 {
-            for j in 0..=2 {
-                if row + i > 0
-                    && col + j > 0
-                    && self.scene.read_buf.get(row + i - 1, col + j - 1) == Some(&'$')
-                {
-                    count += 1;
+        for i in 0..3 {
+            for j in 0..3 {
+                if i == 1 && j == 1 {
+                    continue;
+                }
+
+                let new_row = row.wrapping_add(i).wrapping_sub(1);
+                let new_col = col.wrapping_add(j).wrapping_sub(1);
+
+                if new_row < self.height && new_col < self.width {
+                    if let Some(&true) = self.get(new_row, new_col) {
+                        count += 1;
+                    }
                 }
             }
         }
         count
+    }
+
+    pub fn update(&mut self) {
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let count = self.neighbour_count(row, col);
+                if *self.get(row, col).unwrap() {
+                    if count < 2 || count > 3 {
+                        self.set_next(row, col, false);
+                        self.population -= 1;
+                    } else {
+                        self.set_next(row, col, true);
+                    }
+                } else {
+                    if count == 3 {
+                        self.set_next(row, col, true);
+                        self.population += 1;
+                    } else {
+                        self.set_next(row, col, false);
+                    }
+                }
+            }
+        }
+        self.tick += 1;
+        self.swap();
+    }
+
+    fn swap(&mut self) {
+        std::mem::swap(&mut self.grid, &mut self.next_grid);
+    }
+
+    pub fn render(&self) {
+        print!("\x1B[2J\x1B[1;1H");
+        println!("seed: {}", self.seed);
+        println!("tick: {}", self.tick);
+        println!("population: {}", self.population);
+        //for row in 0..self.height {
+        //    for col in 0..self.width {
+        //        print!("{} ", self.neighbour_count(row, col));
+        //    }
+        //    println!()
+        //}
+        for row in 0..self.height {
+            for col in 0..self.width {
+                match *self.get(row, col).unwrap() {
+                    true => print!("{}", LIFE),
+                    false => print!("{}", DEATH),
+                }
+            }
+            println!()
+        }
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            self.render();
+            self.update();
+            sleep(Duration::from_millis(1000));
+            if self.population == 0 {
+                self.render();
+                break;
+            }
+        }
     }
 }
--- game-of-life/src/main.rs
+++ game-of-life/src/main.rs
@@ -1,6 +1,12 @@
 use game_of_life::conway::Conway;
+use std::env;
 
 fn main() {
-    let conway = Conway::new(0, 0, 32, 32);
-    println!("{}", conway.neighbour_count(0, 0));
+    env::set_var("RUST_BACKTRACE", "FULL");
+    let mut conway = Conway::new(2, 50, 10, 10);
+    conway.random_generation();
+    //conway.set(1, 0, true);
+    //conway.set(1, 1, true);
+    //conway.set(1, 2, true);
+    conway.run();
 }
